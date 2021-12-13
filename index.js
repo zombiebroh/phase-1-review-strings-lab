883c5b06ff58229419609029a19275eb3169932d
Gehrig Barnes <gehrigbarnes@gmail.com>
2021-12-13 11:10:26 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code in this file!
+const currentUser = "Gehrig";
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toLocaleUpperCase();
+const shortGreeting = `Welcome, ${currentUser.charAt(0)}!`;
